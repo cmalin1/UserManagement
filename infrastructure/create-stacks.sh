ea60bc3653b5dadded6a1a7d2e7d4c3c2f012fdc
Cathie <cmalin1@madisoncollege.edu>
2022-08-10 17:01:09 -0500

    Updating infrastructure

---
+++ infrastructure/create-stacks.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+aws cloudformation deploy --stack-name user-management-vpc --template-file ./vpc.yaml
+
+aws cloudformation deploy --stack-name user-management-security --template-file ./security.yaml --capabilities CAPABILITY_IAM 
+
+aws cloudformation deploy --stack-name user-management-web --template-file ./webserver.yaml
---
+++ infrastructure/delete-stacks.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+aws cloudformation delete-stack --stack-name user-management-web
+aws cloudformation wait stack-delete-complete --stack-name user-management-web
+aws cloudformation delete-stack --stack-name user-management-security 
+aws cloudformation wait stack-delete-complete --stack-name user-management-security
+aws cloudformation delete-stack --stack-name user-management-vpc 
+aws cloudformation wait stack-delete-complete --stack-name user-management-vpc
+
+
