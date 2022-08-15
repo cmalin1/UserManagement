2cf4992b898f2175bfac6b2c26eabc3756bf84ea
Cathie <cmalin1@madisoncollege.edu>
2022-08-15 14:07:50 -0500

    Adding Jenkins

--- infrastructure/delete-stacks.sh
+++ infrastructure/delete-stacks.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
-aws cloudformation delete-stack --stack-name jenkins-server
 aws cloudformation delete-stack --stack-name user-management-web
 aws cloudformation wait stack-delete-complete --stack-name user-management-web
 aws cloudformation delete-stack --stack-name user-management-security 
 aws cloudformation wait stack-delete-complete --stack-name user-management-security
-aws cloudformation delete-stack --stack-name user-management-vpc 
-aws cloudformation wait stack-delete-complete --stack-name user-management-vpc
 
 
