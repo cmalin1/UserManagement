2b45e971e87aad64dce8178c4459e15fca92264a
Cathie <cmalin1@madisoncollege.edu>
2022-08-15 14:52:29 -0500

    adding region

--- infrastructure/create-stacks.sh
+++ infrastructure/create-stacks.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-aws cloudformation deploy --stack-name user-management-vpc --template-file ./vpc.yaml
+aws cloudformation deploy --stack-name user-management-vpc --template-file ./vpc.yaml --region us-east-1
 
-aws cloudformation deploy --stack-name user-management-security --template-file ./security.yaml --capabilities CAPABILITY_IAM 
+aws cloudformation deploy --stack-name user-management-security --template-file ./security.yaml --capabilities CAPABILITY_IAM --region us-east-1
 
-aws cloudformation deploy --stack-name user-management-web --template-file ./webserver.yaml
+aws cloudformation deploy --stack-name user-management-web --template-file ./webserver.yaml --region us-east-1
 
-aws cloudformation deploy --stack-name jenkins-server --template-file ./jenkins.yaml --capabilities CAPABILITY_IAM 
+aws cloudformation deploy --stack-name jenkins-server --template-file ./jenkins.yaml --capabilities CAPABILITY_IAM --region us-east-1
