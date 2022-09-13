b9967d744e9024e0bd15e57019040aef0b9c158b
Cathie <cmalin1@madisoncollege.edu>
2022-09-13 12:15:29 -0500

    Updaging files

--- infrastructure/delete-stacks.sh
+++ infrastructure/delete-stacks.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
-aws cloudformation delete-stack --stack-name user-management-db
-aws cloudformation wait stack-delete-complete --stack-name user-management-db
-aws cloudformation delete-stack --stack-name user-management-web
-aws cloudformation wait stack-delete-complete --stack-name user-management-web
-aws cloudformation delete-stack --stack-name user-management-security 
-aws cloudformation wait stack-delete-complete --stack-name user-management-security
+aws cloudformation delete-stack --stack-name user-management-db-dev
+
+aws cloudformation delete-stack --stack-name user-management-web-dev
+
+aws cloudformation delete-stack --stack-name user-management-db-staging
+
+aws cloudformation delete-stack --stack-name user-management-web-staging
+
 
 
 #comment this section out when in the middle of class
-aws cloudformation delete-stack --stack-name jenkins-server 
-aws cloudformation wait stack-delete-complete --stack-name jenkins-server
-aws cloudformation delete-stack --stack-name user-management-ip
-aws cloudformation wait stack-delete-complete --stack-name user-management-ip
-aws cloudformation delete-stack --stack-name user-management-vpc
-aws cloudformation wait stack-delete-complete --stack-name user-management-vpc
+#aws cloudformation delete-stack --stack-name user-management-security 
+#aws cloudformation wait stack-delete-complete --stack-name user-management-security
+#aws cloudformation delete-stack --stack-name jenkins-server 
+#aws cloudformation wait stack-delete-complete --stack-name jenkins-server
+#aws cloudformation delete-stack --stack-name user-management-ip
+#aws cloudformation wait stack-delete-complete --stack-name user-management-ip
+#aws cloudformation delete-stack --stack-name user-management-vpc
+#aws cloudformation wait stack-delete-complete --stack-name user-management-vpc
