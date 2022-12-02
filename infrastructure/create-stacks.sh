421fc654cc53c91d5443f57fe088e49fe5de9ee3
Cathie <cmalin1@madisoncollege.edu>
2022-12-02 14:39:03 -0600

    Working on week 13 assignment

--- infrastructure/create-stacks.sh
+++ infrastructure/create-stacks.sh
@@ -5,8 +5,8 @@ aws cloudformation deploy --stack-name user-management-ip --template-file ./ip.y
 
 aws cloudformation deploy --stack-name user-management-security --template-file ./security.yaml --capabilities CAPABILITY_NAMED_IAM --region us-east-1
 
-aws cloudformation deploy --stack-name user-management-web-dev --template-file ./webserver.yaml --region us-east-1
+aws cloudformation deploy --stack-name user-management-web-dev --template-file ./webserver-bg.yaml --region us-east-1
 
 aws cloudformation deploy --stack-name jenkins-server --template-file ./jenkins.yaml --capabilities CAPABILITY_IAM --region us-east-1
 
-aws cloudformation deploy --stack-name user-management-db-dev --template-file  ./db.yaml --capabilities CAPABILITY_IAM --region us-east-1
+#aws cloudformation deploy --stack-name user-management-db-dev --template-file  ./db.yaml --capabilities CAPABILITY_IAM --region us-east-1
